6bfcfc6d2a3388a184d50c07f67a8db2e8273844
Adrian Ukwunna <adrian@Adrians-MacBook-Pro.local>
2023-09-05 10:43:35 +0100

    create tweetcomp, buttoncomp and textarea height, profileimage, sidenav and update next config

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,18 +1,47 @@
-import React from "react";
+"use client";
+
+import { useState, useRef, useLayoutEffect, useCallback } from "react";
 import ProfileImage from "./ProfileImage";
 import Button from "./Button";
 import { useSession } from "next-auth/react";
 
-const NewTweetForm = () => {
-  const session = useSession();
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (textArea == null) return;
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
 
+export default function NewTweetForm() {
+  const session = useSession();
   if (session.status !== "authenticated") return;
+
+  return <Form />;
+}
+
+function Form() {
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+
+  if (session.status !== "authenticated") return null;
 
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
         <ProfileImage src={session.data.user.image} />
         <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?"
         ></textarea>
@@ -20,6 +49,4 @@ const NewTweetForm = () => {
       <Button className="self-end">Tweet</Button>
     </form>
   );
-};
-
-export default NewTweetForm;
+}
